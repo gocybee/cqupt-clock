70c17a99d6567b4992f6b6e306f895504c609d17
StellarisW <744637972@qq.com>
2022-08-15 17:05:54 +0800

    style: change

--- clock/cookie.py
+++ clock/cookie.py
@@ -4,7 +4,9 @@ import browser_cookie3
 
 
 def get(name):
-    cj = browser_cookie3.chrome('selenium/Default/Network/Cookies', 'ids.cqupt.edu.cn', 'selenium/Local State')
+    cj = browser_cookie3.chrome('./clock/cookies/Default/Network/Cookies',
+                                'ids.cqupt.edu.cn',
+                                './clock/cookies/Local State')
     return cj.__dict__.get('_cookies')['ids.cqupt.edu.cn']['/authserver'][name]
 
 
