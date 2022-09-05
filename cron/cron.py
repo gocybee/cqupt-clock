10d92e9440e9513689b14f8018ee256b88b12e28
StellarisW <744637972@qq.com>
2022-09-05 18:14:19 +0800

    fix: env err

--- cron/cron.py
+++ cron/cron.py
@@ -7,7 +7,7 @@ import requests
 port = os.getenv("CLOCK_PORT")
 if port is None:
     port = 8089
-do_url = f'http://localhost:{port}/do'
+do_url = f'http://localhost:{str(port)}/do'
 
 
 def my_job(**kwargs):
@@ -20,14 +20,18 @@ def my_job(**kwargs):
 
 if __name__ == '__main__':
     # 读取文件
-    with open('info.json', 'r+', encoding='utf-8') as f:
+    with open('./cron/info.json', 'r+', encoding='utf-8') as f:
         j = json.load(f)
     hour = os.getenv("CRON_HOUR")
     if hour is None:
         hour = 12
+    else:
+        hour = int(hour)
     jitter = os.getenv("CRON_JITTER")
     if jitter is None:
         jitter = 3600
+    else:
+        jitter = int(jitter)
     timezone = zoneinfo.ZoneInfo("Asia/Shanghai")
     scheduler = BlockingScheduler()
     scheduler.add_job(my_job, 'cron', kwargs=j, hour=hour, jitter=jitter,
