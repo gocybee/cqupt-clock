f447a04fb7c4e0ba6eab0ba86680b113aaf17ed0
StellarisW <744637972@qq.com>
2022-09-05 18:34:37 +0800

    feat: develop read info.json function

--- cron/cron.py
+++ cron/cron.py
@@ -10,30 +10,31 @@ if port is None:
 do_url = f'http://localhost:{str(port)}/do'
 
 
-def my_job(**kwargs):
-    res = requests.post(
+def my_job():
+    with open('./cron/info.json', 'r+', encoding='utf-8') as f:
+        j = json.load(f)
+    _ = requests.post(
         url=do_url,
-        data=kwargs
+        data=j
     )
-    print(res.text)
 
 
 if __name__ == '__main__':
     # 读取文件
-    with open('./cron/info.json', 'r+', encoding='utf-8') as f:
-        j = json.load(f)
     hour = os.getenv("CRON_HOUR")
     if hour is None:
         hour = 12
     else:
         hour = int(hour)
+
     jitter = os.getenv("CRON_JITTER")
     if jitter is None:
         jitter = 3600
     else:
         jitter = int(jitter)
+
     timezone = zoneinfo.ZoneInfo("Asia/Shanghai")
     scheduler = BlockingScheduler()
-    scheduler.add_job(my_job, 'cron', kwargs=j, hour=hour, jitter=jitter,
+    scheduler.add_job(my_job, 'cron', hour=hour, jitter=jitter,
                       timezone=timezone)
     scheduler.start()
